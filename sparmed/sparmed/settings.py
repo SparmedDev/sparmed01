71259f84e9b5349a3edc86f430c169876d54cf5c
Rami Ahmed Bock <rami@alphastagestudios.com>
2014-08-25 11:33:59 +0000

    Added Under Construction placeholder page

--- sparmed/sparmed/settings.py
+++ sparmed/sparmed/settings.py
@@ -10,73 +10,103 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..').replace('\\', '/')
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-v1cc_typi#%q&0ss(8apt4h^&0jhpy((6t_6j+_dw)k0v7qwe'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-TEMPLATE_DEBUG = True
+HTTPS = False
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+ADMINS = (
+  ('RamiAhmed', 'rami@alphastagestudios.com'),
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+MANAGERS = ADMINS
+
+# Django Sites ID
+SITE_ID = 1
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.herokuapp.com').split(':')
+
+from postgresify import postgresify
+DATABASES = postgresify()
+
+from memcacheify import memcacheify
+CACHES = memcacheify()
+
+TIME_ZONE = 'Europe/Copenhagen'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = False
+USE_L10N = True
+USE_TZ = True
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'mediafiles')
+MEDIA_URL = STATIC_URL + 'media/'
+
+from django.utils.crypto import get_random_string
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_string(50, "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"))
+
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+  'django.core.context_processors.request',
+  'django.contrib.auth.context_processors.auth',
+  'django.contrib.messages.context_processors.messages',
 )
 
 ROOT_URLCONF = 'sparmed.urls'
 
 WSGI_APPLICATION = 'sparmed.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+# Grappelli Admin Site Settings
+GRAPPELLI_ADMIN_TITLE = "Admin | Sparmed.dk"
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+GRAPPELLI = (
+  'grappelli',
+)
 
-STATIC_URL = '/static/'
+DJANGO_APPS = (
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.messages',
+  'django.contrib.sessions',
+  'django.contrib.sites',
+  'django.contrib.sitemaps',
+  'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = (
+  'gunicorn',
+  #'compressor',
+  #'analytical',
+  #'jquery',
+  'bootstrap3',
+  #'disqus',
+  #'embed_video',
+  #'taggit',
+  #'robots',
+  #'storages',
+  #'wysihtml5',
+  #'collectfast',
+)
+
+LOCAL_APPS = (
+  'sparmed',
+)
+
+INSTALLED_APPS = GRAPPELLI + DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
+MIDDLEWARE_CLASSES = (
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
--- sparmed/sparmed/urls.py
+++ sparmed/sparmed/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 
+from sparmed import views
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sparmed.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.home, name='home'),
+    url(r'^home/', views.home),
+                       
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
