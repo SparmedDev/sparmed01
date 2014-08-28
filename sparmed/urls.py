8489eb8b869ce834e41b47eeaf52c3258cd5f747
Rami Ahmed Bock <rami@alphastagestudios.com>
2014-08-28 10:16:29 +0000

    Attempted to implement forgot/reset/change password

--- sparmed/urls.py
+++ sparmed/urls.py
@@ -50,7 +50,12 @@ urlpatterns = patterns('',
 
                        url(r'^products/(?P<category_slug>[\w\-]+)/(?P<product_slug>[\w\-]+)/$', 'shop.views.details'),
                        url(r'^products/(?P<slug>[\w\-]+)/$', 'shop.views.products'),
-                       url(r'^products/$', 'shop.views.products', name='products'),                       
+                       url(r'^products/$', 'shop.views.products', name='products'), 
+                       
+                       url(r'^user/password/reset/$', 'django.contrib.auth.views.password_reset', {'post_reset_redirect' : '/user/password/reset/done/'}, name="password_reset"),
+                       url(r'^user/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+                       url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'post_reset_redirect' : '/user/password/done/'}),
+                       url(r'^user/password/done/$', 'django.contrib.auth.views.password_reset_complete'),                       
 
                        url(r'^grappelli/', include('grappelli.urls')),
                        url(r'^admin/', include(admin.site.urls)),
