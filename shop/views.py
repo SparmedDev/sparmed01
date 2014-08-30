dafbe3c2d7aef58f069626273ef6a0215ace2fd6
Rami Ahmed Bock <rami@alphastagestudios.com>
2014-08-30 15:46:07 +0000

    Minor improvements to products and details pages

--- shop/views.py
+++ shop/views.py
@@ -6,19 +6,19 @@ from shop.models import Product, Category
 # Create your views here.
 @never_cache
 def products(request, slug="0"):
-    try:
-        category = Category.objects.get(slug=slug)    
-    except Category.DoesNotExist:
-        category = Category.objects.get(id=1)
-        
     categories = Category.objects.all()
+    
+    try:
+        category = categories.get(slug=slug)    
+    except Category.DoesNotExist:
+        category = categories.get(id=1)     
   
     return render(request, 'shop/products.html', {'categories':categories, 'category':category})
 
 @never_cache
 def details(request, category_slug, product_slug):
   category = get_object_or_404(Category, slug=category_slug)
-  product = get_object_or_404(Product, slug=product_slug)
+  product = category.get(slug=product_slug)
   images = product.images.all()
 
   return render(request, 'shop/details.html', {'category':category, 'product':product, 'images':images})
