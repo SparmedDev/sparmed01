00b2a8ce3214eaa0fca7991d5cd5861ed1e70c9b
Rami Ahmed Bock <rami@alphastagestudios.com>
2015-01-05 23:42:06 +0000

    Testing different memcache backend

--- sparmed/settings.py
+++ sparmed/settings.py
@@ -37,10 +37,38 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.herokuapp.com').split(':')
 from postgresify import postgresify
 DATABASES = postgresify()
 
+## MemCachier Settings
+## ===================
+def get_cache():
+  # We do this complicated cache defenition so that on a local machine (where
+  # MEMCACHIER_SERVERS won't be defined), the try fails and so we use the
+  # inbuilt local memory cache of django.
+  try:
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+    os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+    os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+    return {
+      'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'TIMEOUT': 500,
+        'BINARY': True,
+        'OPTIONS': { 'tcp_nodelay': True }
+      }
+    }
+  except:
+    # Use django local development cache (for local development).
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
+
+CACHES = get_cache()
+
 if not DEBUG:
   # Memcached
-  from memcacheify import memcacheify
-  CACHES = memcacheify()
+  #from memcacheify import memcacheify
+  #CACHES = memcacheify()  
   
   #SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
   #SESSION_COOKIE_AGE = 1209600 # 2 weeks in seconds
--- sparmed/wsgi.py
+++ sparmed/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
+# Fix django closing connection to MemCachier after every request (#11331)
+from django.core.cache.backends.memcached import BaseMemcachedCache
+BaseMemcachedCache.close = lambda self, **kwargs: None
