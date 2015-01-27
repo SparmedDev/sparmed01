3554a6074052a58ae8bc925a71408278ae3a99f6
Rami Ahmed Bock <rami@alphastagestudios.com>
2015-01-27 13:06:28 +0000

    Testing e-conomics integration

--- sparmed/views.py
+++ sparmed/views.py
@@ -7,37 +7,26 @@ from shop.models import Category
 
 def home(request):
   news_posts = NewsPost.objects.all()
-
   return render(request, "index.html", {'news_posts':news_posts})
 
 def about(request):
-
   return render(request, "about.html")
 
 def contact(request):
-
   return render(request, "contact.html")
 
-def terms_conditions(request):
-  
+def terms_conditions(request):  
   return render(request, "terms_conditions.html")
 
-def privacy_policy(request):
-  
+def privacy_policy(request):  
   return render(request, "privacy_policy.html")
 
-#from suds.client import Client
 @never_cache
 @login_required
 def inventory(request):
-  categories = Category.objects.all()      
-  
-  #url = 'api.e-conomic.com/secure/api1/economicwebservice.asmx'
-  #client = Client(url)
-
+  categories = Category.objects.all()  
   return render(request, "inventory.html", {'categories':categories,})
 
 @login_required
-def distributor_login(request):
-  
+def distributor_login(request):  
   return render(request, "distributor_login.html")
