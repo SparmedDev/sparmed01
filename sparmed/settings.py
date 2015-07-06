950e00117fdd83c7549592d86bb0b9b1ecb083f6
Rami Ahmed Bock <rami@alphastagestudios.com>
2015-07-06 21:32:39 +0000

    Use I18N True

--- sparmed/settings.py
+++ sparmed/settings.py
@@ -46,7 +46,7 @@ MEMCACHEIFY_USE_LOCAL=DEBUG
 
 TIME_ZONE = 'Europe/Copenhagen'
 LANGUAGE_CODE = 'en-us'
-USE_I18N = False
+USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 FIRST_DAY_OF_WEEK = 1 # start week on Monday
