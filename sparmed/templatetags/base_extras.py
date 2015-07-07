c2569bdf81e626541741b65d102816002df2dc10
Rami Ahmed Bock <rami@alphastagestudios.com>
2015-07-07 21:21:31 +0000

    Tried to fix next input in lang set

--- sparmed/templatetags/base_extras.py
+++ sparmed/templatetags/base_extras.py
@@ -44,6 +44,22 @@ def productnavactive(request, name):
     
     return ""
 
+class TranslatedURL(template.Node):
+    def __init__(self, language):
+        self.language = language
+    def render(self, context):
+        view = resolve(context['request'].path)
+        request_language = translation.get_language()
+        translation.activate(self.language)
+        url = reverse(view.url_name, args=view.args, kwargs=view.kwargs)
+        translation.activate(request_language)
+        return url
+
+@register.tag(name='translate_url')
+def do_translate_url(parser, token):
+    language = token.split_contents()[1]
+    return TranslatedURL(language)  
+  
 @register.tag
 def settings_value(parser, token):
     try:
