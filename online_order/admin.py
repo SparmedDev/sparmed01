f01272430602031bbca7c19065872c0d609b559f
Rami Ahmed Bock <rami@alphastagestudios.com>
2015-07-10 01:14:41 +0000

    Fixed user adin form

--- online_order/admin.py
+++ online_order/admin.py
@@ -2,62 +2,30 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserChangeForm, UserCreationForm
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from online_order.models import SparmedUser
 # Register your models here.
 
-class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)  
-    
-    class Meta:
-        model = SparmedUser
-        fields = ('name', 'company_name', 'email', 'contact_person_name')
-                  
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user                  
-                  
-class UserChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = SparmedUser
-        fields = ('name', 'company_name', 'email', 'password', 'contact_person_name', 'is_active', 'is_admin',)
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]                  
-      
 class SparmedUserChangeForm(UserChangeForm):
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = SparmedUser
-        fields = ('name', 'company_name', 'country', 'address', 'city', 'postal_code', 'email', 'contact_person_name', 'contact_telephone', 'password',)
-
-    def clean_password(self):
-        return self.initial["password"]   
+  
+  def __init__(self, *args, **kwargs):
+      super(SparmedUserChangeForm, self).__init__(*args, **kwargs)
+      self.fields['name'].required = True
+      self.fields['company_name'].required = True
+      self.fields['country'].required = True
+      self.fields['address'].required = True
+      self.fields['city'].required = True
+      self.fields['postal_code'].required = True
+      self.fields['email'].required = True
+      self.fields['contact_person_name'].required = True
+      self.fields['contact_telephone'].required = True
 
       
 class SparmedUserAdmin(UserAdmin):
     # The forms to add and change user instances
-    form = UserChangeForm
+    form = SparmedUserChangeForm
     add_form = UserCreationForm
 
     # The fields to be used in displaying the User model.
@@ -67,21 +35,26 @@ class SparmedUserAdmin(UserAdmin):
     list_filter = ('is_admin', 'country',)
     fieldsets = (
         (None, {'fields': ('name', 'email', 'password')}),
-        ('Personal info', {'fields': ('company_name', 'country', 'address', 'city', 'postal_code', 'contact_person_name', 'contact_telephone',)}),
-        ('Permissions', {'fields': ('is_active', 'is_admin','is_superuser',)}),
+        (_('Personal info'), {'fields': ('company_name', 'country', 'address', 'city', 'postal_code', 'contact_person_name', 'contact_telephone',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_admin', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('name', 'email', 'company_name', 'country', 'address', 'city', 'postal_code', 'contact_person_name', 'contact_telephone', 'password1', 'password2')}
+            'fields': ('name', 'company_name', 'country', 'address', 'city', 'postal_code', 'contact_person_name', 'contact_telephone')}
         ),
     )
+    
     search_fields = ('name', 'company_name', 'country', 'email', 'contact_person_name', 'address')
     ordering = ('company_name', 'country', 'name',)
     filter_horizontal = () 
-
     
 admin.site.register(SparmedUser, SparmedUserAdmin)
 #admin.site.unregister(Group)
+
+#from django.contrib.auth.models import Permission
+#admin.site.register(Permission)
