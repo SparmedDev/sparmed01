870da2dc63eab9efd4b91d0354bc3552f202eb9f
Rami Ahmed Bock <ramiahmedbock@gmail.com>
2016-08-09 18:18:20 +0000

    fixed exception

--- cart/views.py
+++ cart/views.py
@@ -6,21 +6,22 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 
 from cart import Cart
+from cart import ItemDoesNotExist
 from shop.models import Product, Category
 from online_order.models import InventoryAddToCartForm
 
 @login_required
 def add_to_cart(request):
     if request.method == 'POST':
-        product_pk = request.POST.get("product_pk")              
-        quantity = request.POST.get("quantity", 1) 
-        
+        product_pk = request.POST.get("product_pk")
+        quantity = request.POST.get("quantity", 1)
+
         product = get_object_or_404(Product, pk=product_pk)
         cart = Cart(request)
         cart.add(product, quantity)
-    
+
         return HttpResponse('success')
-    
+
     return HttpResponse('error')
 
 @login_required
@@ -29,9 +30,9 @@ def remove_from_cart(request, object_id):
     cart = Cart(request)
     try:
         cart.remove(product)
-    except Item.DoesNotExist:
+    except ItemDoesNotExist:
         pass
-    
+
     return HttpResponseRedirect(reverse('online_order.views.order_online'))
 
 @login_required
@@ -46,10 +47,10 @@ def set_quantity_on_product(request):
                     cart.set_product_quantity(product, value)
 
     return HttpResponseRedirect(reverse('online_order.views.order_online'))
-  
-@login_required  
+
+@login_required
 def clear_cart(request):
     cart = Cart(request)
     cart.clear()
-  
+
     return HttpResponseRedirect(reverse('online_order.views.order_online'))
